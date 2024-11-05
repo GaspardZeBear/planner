f67e791fa312e02d467a3ac9038b216c1be8d0e4
gaspardzebear <ubu@LAPTOP-V5BH8JC1.localdomain>
2024-11-05 12:38:33 +0100

    Birth

---
+++ src/test/java/com/gzb/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzb.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
