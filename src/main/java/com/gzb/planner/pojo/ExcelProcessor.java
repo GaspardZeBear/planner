04fb5bacedf89a56fe92125e87e9b303edf82a3f
gaspardzebear <ubu@LAPTOP-V5BH8JC1.localdomain>
2024-11-07 09:19:51 +0100

    Formula in planning

--- src/main/java/com/gzb/planner/pojo/ExcelProcessor.java
+++ src/main/java/com/gzb/planner/pojo/ExcelProcessor.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;  
 import java.util.HashMap;
-import org.apache.poi.ss.usermodel.Cell;  
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;  
 import org.apache.poi.xssf.usermodel.XSSFSheet;  
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
-import com.gzb.planner.pojo.PlanningItem; 
-
+import com.gzb.planner.pojo.PlanningItem;
 
 public class ExcelProcessor {
 
@@ -28,14 +30,14 @@ public class ExcelProcessor {
     try  {  
       FileInputStream fis = new FileInputStream(new File(excelFile));
       XSSFWorkbook wb = new XSSFWorkbook(fis); 
-      getColors(wb);
-      getPlanning(wb);
+      computeColors(wb);
+      computePlanning(wb);
     } catch(Exception e)  {  
       e.printStackTrace();  
     } 
   }
 
-  public void getColors(XSSFWorkbook wb) {
+  public void computeColors(XSSFWorkbook wb) {
     XSSFSheet sheet = wb.getSheet("Colors"); 
     if ( sheet == null ) { 
       return;     
@@ -53,7 +55,7 @@ public class ExcelProcessor {
     }     
   }
 
-  public void getPlanning(XSSFWorkbook wb) {
+  public void computePlanning(XSSFWorkbook wb) {
     XSSFSheet sheet = wb.getSheet("Planning"); 
     if ( sheet == null ) { 
       sheet = wb.getSheetAt(0);     
@@ -61,6 +63,7 @@ public class ExcelProcessor {
     Iterator<Row> itr = sheet.iterator();    //iterating over excel file
     Row row1=itr.next();  // skip first line
     itemList = new ArrayList<PlanningItem>();
+    FormulaEvaluator evaluator=wb.getCreationHelper().createFormulaEvaluator();
     while (itr.hasNext())  {  
       Row row = itr.next(); 
       PlanningItem item=new PlanningItem(); 
@@ -73,9 +76,25 @@ public class ExcelProcessor {
             break;  
           case NUMERIC:    //field that represents number cell type  
             item.setIdx(cell.getColumnIndex(),String.valueOf(cell.getNumericCellValue()));  
-            break;  
+            break;
+          case FORMULA:    //field that represents number cell type  
+            CellType cellType=evaluator.evaluateFormulaCell(cell);
+            CellValue cellValue=evaluator.evaluate(cell);
+            switch(cellType) {
+              case STRING:  
+                item.setIdx(cell.getColumnIndex(),String.valueOf(cellValue.getStringValue()));  
+              break;
+              case NUMERIC:    //field that represents number cell type  
+                item.setIdx(cell.getColumnIndex(),String.valueOf(cellValue.getNumberValue()));  
+                break; 
+              default: 
+                item.setIdx(cell.getColumnIndex(),"???");
+                break; 
+            }
+            break;
           default: 
-            item.setIdx(cell.getColumnIndex(),"");
+            item.setIdx(cell.getColumnIndex(),"?");
+            System.out.println(" cellType " + String.valueOf(cell.getCellType()));
             break;
         } 
       }  
