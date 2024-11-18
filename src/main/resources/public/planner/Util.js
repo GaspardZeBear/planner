4d71e1e8987ef4e44934b7fa5bd133a558aa0bd6
gaspardzebear <ubu@LAPTOP-V5BH8JC1.localdomain>
2024-11-18 17:24:01 +0100

    Adding action buttons to select box

--- src/main/resources/public/planner/Util.js
+++ src/main/resources/public/planner/Util.js
@@ -2,16 +2,44 @@ class Util {
  
   
 //---------------------------------------------------------------------------------------------------------------------------------------------
-static buildFilesList(selector,pFilesList,action) {
-  console.log("fFiles " + JSON.stringify(filesList))
-  var filesList = document.createElement("select");
-  filesList.id=selector;
-  //document.querySelector("#" + selector).removeChild()
+static buildFilesList(selector,pFilesList,actions) {
+  console.log("fFiles " + JSON.stringify(pFilesList))
+  
+  //cleanup all the div
   const element = document.querySelector("#" + selector);
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
-  document.querySelector("#" + selector).appendChild(filesList)
+
+  
+  var selectedItem=document.createElement("Text")
+  selectedItem.id=selector+"Selected"
+  document.querySelector("#" + selector).appendChild(selectedItem)
+
+  // Create the action buttons and associated listener
+  console.log("Buttons creation ") 
+  for (let action of actions) {
+    console.log("Button creation " + action["text"]) 
+    var button=document.createElement("Button")
+    button.textContent=action["text"]
+    button.id=selector+"Button"+action["text"]
+    console.log("Button created " + action["text"]) 
+    document.querySelector("#" + selector).appendChild(button)
+    document.querySelector("#" + selector + "Button" +action["text"]).addEventListener('click', function (e) {
+        console.log("Button hit " + action["text"]) 
+        let sel=document.getElementById(selector + "Selected")
+        if ( sel.value == null ) {
+          return
+        }
+        action["action"](sel.value)
+    },true)
+  }
+  console.log("Buttons created ") 
+
+  // build select box
+  console.log(" Building select box")
+  var filesList = document.createElement("select");
+  filesList.id=selector+"Select";
   let selected = false
   var option=document.createElement("option")
   option.value=""
@@ -20,7 +48,7 @@ static buildFilesList(selector,pFilesList,action) {
   selected=false
   filesList.appendChild(option)
   for ( let xfile of pFilesList.sort() ){
-    console.log(" File " + xfile)
+    console.log(" File " + xfile + " added to select box")
     var option=document.createElement("option")
     option.value=xfile
     option.text=xfile
@@ -28,18 +56,19 @@ static buildFilesList(selector,pFilesList,action) {
     selected=false
     filesList.appendChild(option)
   }
-  document.querySelector("#" + selector).addEventListener('change', function (e) { 
-    console.log("File selected change " + e.target.value)
-    if ( e.target.value.length > 0 ) {
-      action(e.target.value)
-    }
-    },true);
-  document.querySelector("#" + selector).addEventListener('click', function (e) { 
-    console.log("File selected click " + e.target.value)
-    if ( e.target.value.length > 0 ) {
-      action(e.target.value)
-    }
-    },true);
+  document.querySelector("#" + selector).appendChild(filesList)
+  console.log(" Select box built")
 
-  }
-}
+  // Add select box listeners
+  document.querySelector("#" + selector + "Select").addEventListener('change', function (e) { 
+      console.log("File selected change " + e.target.value)
+      console.log("index " + e.target.selectedIndex)
+      if ( e.target.value.length > 0 ) {
+        document.getElementById(selector + "Selected").value=e.target.value
+        //action(e.target.value)
+      }
+    },true);
+    
+  } // EoMethod
+
+} //EoClass
