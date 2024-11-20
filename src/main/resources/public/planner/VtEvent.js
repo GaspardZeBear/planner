84a21b41b0d7535927895add3569406e123ff298
gaspardzebear <ubu@LAPTOP-V5BH8JC1.localdomain>
2024-11-20 06:25:01 +0100

    Cosmetic

--- src/main/resources/public/planner/VtEvent.js
+++ src/main/resources/public/planner/VtEvent.js
@@ -10,13 +10,27 @@ class VtEvent {
     this.processing=this.kind + ";" + note
   }
 
-  getKind() { return(this.kind) }
-  setKind(kind) { this.kind=kind }
-  getNote() { return(this.note) }
-  getWhen() { return(this.when) }
+  //-----------------------------------------------------------------------------
+  getKind() { 
+    return(this.kind) 
+  }
+  //-----------------------------------------------------------------------------
+    setKind(kind) { 
+    this.kind=kind 
+  }
+  //-----------------------------------------------------------------------------
+  getNote() { 
+    return(this.note) 
+  }
+  //-----------------------------------------------------------------------------
+  getWhen() { 
+    return(this.when)
+  }
+  //-----------------------------------------------------------------------------
   addProcessing(msg) {
     this.processing = this.processing + "<br/>" + msg
   }
+  //-----------------------------------------------------------------------------
   getProcessing() {
     return(this.processing)
   }
